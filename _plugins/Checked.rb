6adb17800307b59a70aee87ce49ec14a8693d841
Jaroslav Holub <jholub@gmail.com>
2015-07-03 12:56:20 +0200

    Using exported site in gh-pages branch (due to jekyll plugins usage)

--- _plugins/Checked.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  module Checked
-    def checked(text)
-      text.gsub(%r{\[x\]}i, '<span class="ballot_box_with_check"></span>').gsub(%r{\[ \]}i, '<span class="ballot_box"></span>')
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::Checked)
